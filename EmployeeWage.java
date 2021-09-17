b6da9662bbb8cf26f9780c75e2d905e06f4887af
Ashish Sahu <aajisahu@gmail.com>
2021-09-17 23:03:58 +0530

    adding employee is present or absent

---
+++ EmployeeWage.java
@@ -0,0 +1,16 @@
+package EmployeeWages;
+
+public class EmployeeWage {
+
+			public static void main(String[] args) {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+			if (empCheck == 1) {
+				System.out.println("Employee is Present");
+			}else {
+			System.out.println("Employee is Absent");
+			}
+		}
+	
+	
+	
+}
