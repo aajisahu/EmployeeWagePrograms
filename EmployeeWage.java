32295cad0536d4e8291b23bfb3d9911f4a8dddb6
Ashish Sahu <aajisahu@gmail.com>
2021-09-17 23:20:33 +0530

    solving using calculating of a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,6 +5,7 @@ public class EmployeeWage {
 		int PartTimeHours = 4;
 		int FullDay = 8;
 		int WageParHour = 20;
+		int WorkingDayParMonth = 20;
 		final int empPresent = 1;
 		final int empPartTime = 2;
 		int dailyWage = 0;
@@ -13,19 +14,19 @@ public class EmployeeWage {
 		
 		switch (empCheck) {
 		case empPresent:
-			dailyWage = FullDay*WageParHour;
+			dailyWage = FullDay*WageParHour*WorkingDayParMonth;
 			System.out.println("Employee is Present");
-			System.out.println("Employee Daily Wage Is : "+ dailyWage);
+			System.out.println("Employee Daily 20 Day Wage Is : "+ dailyWage);
 			break;
 		case empPartTime:{
-			dailyWage = PartTimeHours*WageParHour;
+			dailyWage = PartTimeHours*WageParHour*WorkingDayParMonth;
 			System.out.println("Employee Part Time is Present");
-			System.out.println("Employee Part Time Wage Is : "+ dailyWage);
+			System.out.println("Employee Part Time 20 Day Wage Is : "+ dailyWage);
 			break;
 		}
 		default:
 			System.out.println("Employee is Absent");
 			break;
-		}
+			}
 	}
 }
